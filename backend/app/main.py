b60c3cf24b540b51a727bff9902c8feda2b2a062
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2025-12-25 14:48:46 +1100

    SCR Number Working

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from sqlalchemy import text
 from .database import engine
-from .routers import InvoiceRouter
+from .routes import invoices
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -17,7 +17,7 @@ app.add_middleware(
 )
 
 # Include routers
-app.include_router(InvoiceRouter.router)
+app.include_router(invoices.router)
 
 # Test DB connection on startup
 @app.on_event("startup")
