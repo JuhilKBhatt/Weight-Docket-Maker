16d9acea6ee9263c6d2954c152202b62bc540198
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2025-12-28 15:52:31 +1100

    Back Button Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // frontend/src/App.jsx
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { Layout, Flex } from 'antd'
+import { Layout } from 'antd' // Removed Flex
 import PageHeader from './components/PageHeader.jsx'
 
 // Importing components for different routes
@@ -16,23 +16,21 @@ const { Header, Content } = Layout
 
 export default function App() {
   return (
-    <Flex gap="middle" wrap>
-      <Layout>
-        <Header>
-          <PageHeader />
-        </Header>
-        <Content>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Home />} /> {/* Fallback route */}
-            <Route path="/docket" element={<DocketMaker />} />
-            <Route path="/InvoiceHome" element={<InvoiceHome />} />
-            <Route path="/new-invoice" element={<NewInvoice mode="new"/>} />
-            <Route path="/edit-invoice/:id" element={<EditInvoice />} />
-            <Route path="/view-invoice" element={<ViewInvoice />} />
-          </Routes>
-        </Content>
-      </Layout>
-    </Flex>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header style={{ display: 'flex', alignItems: 'center', padding: '0 20px', background: 'transparent' }}>
+        <PageHeader />
+      </Header>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} /> {/* Fallback route */}
+          <Route path="/docket" element={<DocketMaker />} />
+          <Route path="/InvoiceHome" element={<InvoiceHome />} />
+          <Route path="/new-invoice" element={<NewInvoice mode="new"/>} />
+          <Route path="/edit-invoice/:id" element={<EditInvoice />} />
+          <Route path="/view-invoice" element={<ViewInvoice />} />
+        </Routes>
+      </Content>
+    </Layout>
   )
 }
